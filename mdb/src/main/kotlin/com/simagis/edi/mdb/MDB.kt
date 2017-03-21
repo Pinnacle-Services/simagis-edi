e0975d99687d01cb14f599b6ad880a23d6a63cb3
Alexei Vylegzhanin <alexei.vylegzhanin@gmail.com>
2017-03-22 00:42:31 +0500

    reimplemented ISA2GridFS.kt

--- mdb/src/main/kotlin/com/simagis/edi/mdb/MDB.kt
+++ mdb/src/main/kotlin/com/simagis/edi/mdb/MDB.kt
@@ -19,13 +19,34 @@ fun MongoClient.grantRolesToUser(user: String, vararg roleToDBs: Pair<String, St
     })
 }
 
+@Suppress("unused")
 object `$` {
     override fun toString(): String = '$'.toString()
     operator fun invoke(name: String): String = '$' + name
+    operator fun plus(name: String): String = '$' + name
 }
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun Document.`+`(key: String, value: Any?): Document = append(key, value)
+
+@Suppress("NOTHING_TO_INLINE", "unused")
+inline fun Document.`+$`(key: String, value: Any?): Document = append(`$`(key), value)
+
+@Suppress("unused")
+inline fun Document.`+$set`(build: Document.() -> Unit): Document {
+    append(`$` + "set", Document().apply {
+        build()
+    })
+    return this
+}
+
+@Suppress("unused")
+inline fun Document.`+$addToSet`(build: Document.() -> Unit): Document {
+    append(`$` + "addToSet", Document().apply {
+        build()
+    })
+    return this
+}
 
 fun Document.`+`(pair: Pair<String, Any?>, vararg pairs: Pair<String, Any?>): Document {
     append(pair.first, pair.second)
@@ -34,3 +55,16 @@ fun Document.`+`(pair: Pair<String, Any?>, vararg pairs: Pair<String, Any?>): Do
 }
 
 inline fun doc(build: Document.() -> Unit) = Document().apply { build() }
+@Suppress("NOTHING_TO_INLINE")
+inline fun doc(_id: Any?) = Document("_id", _id)
+inline fun doc(_id: Any?, build: Document.() -> Unit) = Document("_id", _id).apply { build() }
+
+val Document._id: Any? get() = this["_id"]
+
+operator fun Document?.invoke(vararg keyPath: String): Any? {
+    var res: Any? = this
+    for (key in keyPath) {
+        res = if (res is Document) res[key] else return null
+    }
+    return res
+}
--- toBaseX/src/main/kotlin/com/simagis/edi/mongodb/ISA2GridFS.kt
+++ toBaseX/src/main/kotlin/com/simagis/edi/mongodb/ISA2GridFS.kt
@@ -1,13 +1,21 @@
 package com.simagis.edi.mongodb
 
 import com.mongodb.DB
-import com.mongodb.DBObject
+import com.mongodb.DBRef
+import com.mongodb.client.MongoCollection
 import com.mongodb.gridfs.GridFS
 import com.simagis.edi.basex.ISA
 import com.simagis.edi.basex.get
 import com.simagis.edi.mdb.MDBCredentials
+import com.simagis.edi.mdb.*
+import org.bson.Document
 import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributeView
 import java.security.MessageDigest
+import java.util.*
 
 /**
  * <p>
@@ -24,42 +32,126 @@ fun main(args: Array<String>) {
     val db = DB(mongo, _fs)
 
     val fs = GridFS(db)
+    val fsDatabase = mongo.getDatabase(_fs)
+    val srcCollection = fsDatabase.getCollection("src.files")
+    val isaCollection = fsDatabase.getCollection("src.isa")
 
-    fun File.walk(mode: String): List<File> {
-        return when (mode) {
-            "R" -> walk().toList()
-            "D" -> listFiles()?.asList() ?: emptyList()
-            "F" -> listOf(this)
-            else -> emptyList()
-        }.filter {
-            it.isFile
-        }
-    }
+    val isaIdSet = loadIdSet(isaCollection)
 
-    fun md(): MessageDigest = MessageDigest.getInstance("SHA")
-    fun ByteArray.toHexString(): String = joinToString(separator = "") {
-        Integer.toHexString(it.toInt() and 0xff)
-    }
-
-    val names = mutableSetOf<String>().also { names: MutableSet<String> ->
-        fs.fileList.forEach { file: DBObject ->
-            (file["filename"] as? String?)?.let {
-                names += it
-            }
-        }
-    }
-
-    _path.walk(_mode).forEach {
+    _path.walk(_mode).forEach { srcFile ->
         try {
-            ISA.read(it).forEach { isa ->
-                val byteArray = isa.code.toByteArray(ISA.CHARSET)
-                val name = md().digest(byteArray).toHexString() + ".isa"
-                if (names.add(name)) {
-                    fs.createFile(byteArray.inputStream(), name).save()
+            val srcFileDoc = srcCollection.openSrcFileDoc(srcFile)
+            if (srcFileDoc["status"] == "CREATING") try {
+                ISA.read(srcFile).forEachIndexed { index, isa ->
+                    val byteArray = isa.code.toByteArray(ISA.CHARSET)
+                    val isaDigest = md().digest(byteArray).toHexString()
+                    if (isaIdSet.add(isaDigest)) {
+                        val gridFile = fs.createFile(byteArray.inputStream(), isaDigest + ".isa")
+                        gridFile.save()
+                        isaCollection.insertOne(doc(isaDigest) {
+                            `+`("file", DBRef("fs.files", gridFile.get("_id")))
+                            `+`("src", DBRef("src.files", srcFileDoc["_id"]))
+                            `+`("srcIndex", index)
+                            `+`("type", isa.stat.doc.type)
+                            `+`("date", isa.stat.doc.date)
+                            `+`("time", isa.stat.doc.time)
+                            `+`("status", "NEW")
+                        })
+                        srcCollection.updateOne(
+                                doc(srcFileDoc._id),
+                                doc { `+$addToSet` { `+`("isas", isaDigest) } })
+                    }
                 }
+                srcCollection.updateStatus(srcFileDoc, "NEW")
+            } catch(e: Throwable) {
+                e.printStackTrace()
+                srcCollection.updateStatus(srcFileDoc, "ERROR", e)
             }
-        } catch(e: Exception) {
+        } catch(e: Throwable) {
             e.printStackTrace()
         }
     }
+}
+
+private fun MongoCollection<Document>.updateStatus(doc: Document, status: String, e: Throwable? = null) {
+    updateOne(
+            doc(doc._id),
+            doc {
+                `+$set` {
+                    `+`("status", status)
+                    if (e != null) {
+                        `+`("error", doc {
+                            `+`("class", e.javaClass.name)
+                            `+`("message", e.message)
+                            `+`("stackTrace", StringWriter().let {
+                                PrintWriter(it).use { e.printStackTrace(it) }
+                                it.toString()
+                            })
+
+                        })
+                    }
+                }
+            })
+}
+
+
+private fun MongoCollection<Document>.openSrcFileDoc(file: File): Document {
+    val path = file.absolutePath
+    val srcDigest = file.digest()
+    val foundDoc = find(doc(srcDigest)).first()
+    if (foundDoc == null) {
+        val newDoc = doc(srcDigest) {
+            `+`("paths", listOf(path))
+            `+`("size", file.length())
+            with(Files.getFileAttributeView(file.toPath(), BasicFileAttributeView::class.java).readAttributes()) {
+                `+`("created", Date(creationTime().toMillis()))
+                `+`("modified", Date(lastModifiedTime().toMillis()))
+            }
+            `+`("status", "CREATING")
+        }
+        insertOne(newDoc)
+        return newDoc
+    } else {
+        val updateResult = updateOne(
+                doc(srcDigest),
+                doc { `+$addToSet` { `+`("paths", path) } })
+        if (updateResult.modifiedCount > 0) {
+            return find(doc(srcDigest)).first()
+        } else {
+            return foundDoc
+        }
+    }
+}
+
+private fun loadIdSet(srcCollection: MongoCollection<Document>): MutableSet<String> = mutableSetOf<String>().apply {
+    srcCollection.find().forEach({
+        this += it["_id"] as String
+    })
+}
+
+private fun File.digest(): String = md().apply {
+    inputStream().use {
+        val bytes = ByteArray(4096)
+        while (true) {
+            val len = it.read(bytes)
+            if (len == -1) break
+            update(bytes, 0, len)
+        }
+    }
+}.digest().toHexString()
+
+
+private fun File.walk(mode: String): List<File> = when (mode) {
+    "R" -> walk().toList()
+    "D" -> listFiles()?.asList() ?: emptyList()
+    "F" -> listOf(this)
+    else -> emptyList()
+}.filter {
+    it.isFile
+}
+
+private fun md(): MessageDigest = MessageDigest.getInstance("SHA")
+
+private fun ByteArray.toHexString(): String = joinToString(separator = "") {
+    Integer.toHexString(it.toInt() and 0xff)
 }
