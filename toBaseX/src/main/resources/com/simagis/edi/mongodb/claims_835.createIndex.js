937f43a1692a95a8cfdb0cf27fc942d2c53b5007
Alexei Vylegzhanin <alexei.vylegzhanin@gmail.com>
2017-03-21 11:11:34 -0400

    reimplemented claims_835.createIndex.js

--- toBaseX/src/main/resources/com/simagis/edi/mongodb/claims_835.createIndex.js
+++ toBaseX/src/main/resources/com/simagis/edi/mongodb/claims_835.createIndex.js
@@ -1,5 +1,14 @@
-db.claims_835.createIndex({"acn": 1});
-db.claims_835.createIndex({"procDate": -1});
-db.claims_835.createIndex({"svc.cpt": 1});
-db.claims_835.createIndex({"svc.adj.adjReason": 1});
-db.claims_835.createIndex({"prn": 1});
+(function (collection, indexes) {
+    for (var i = 0; i < indexes.length; i++) collection.createIndex(indexes[i]);
+})(db.claims_835, [
+    {"acn": 1},
+    {"procDate": -1},
+    {"svc.cpt": 1},
+    {"svc.adj.adjReason": 1},
+    {"prn": 1},
+    {"status": 1},
+    {"clmAsk": 1},
+    {"sys": 1},
+    {"svc.cptAsk": 1},
+    {"ref": 1}
+]);
