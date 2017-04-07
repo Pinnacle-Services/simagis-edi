47c3ddcdef343bc2dc6434068c1b5e77f3728f2a
Alexei Vylegzhanin <alexei.vylegzhanin@gmail.com>
2017-04-07 16:24:00 +0500

    added claims_835c.createIndex.js

---
+++ toBaseX/src/main/resources/com/simagis/edi/mongodb/claims_835c.createIndex.js
@@ -0,0 +1,22 @@
+(function (collection, indexes) {
+    for (var i = 0; i < indexes.length; i++) collection.createIndex(indexes[i]);
+})(db.claims_835c, [
+    {"acn": 1},
+    {"sys": 1},
+    {"ref": 1},
+    {"procDate": -1},
+    {"prn": 1},
+    {"status": 1},
+    {"clmAsk": 1},
+    {"svc.cpt": 1},
+    {"svc.cptAsk": 1},
+    {"svc.adj.adjGrp": 1},
+    {"svc.adj.adjReason": 1},
+    {"clmPayTotal": 1},
+    {"clmPay": 1},
+    {"ptnId": 1},
+    // +837
+    {"sendDate": -1},
+    {"dx": 1},
+    {"npi": 1}
+]);
