8eab8ed233bd2b225d2afc5ebd46483c0a647556
Alexei Vylegzhanin <alexei.vylegzhanin@gmail.com>
2017-05-15 22:16:44 +0500

    reimplemented rest-api-demo.html

--- toBaseX/src/main/kotlin/com/simagis/edi/mongodb/ToMongoDB.debug.js
+++ toBaseX/src/main/kotlin/com/simagis/edi/mongodb/ToMongoDB.debug.js
@@ -1,10 +1,22 @@
 db.apiJobs.remove({_id: 'debug'});
 db.apiJobs.insertOne({
     _id: 'debug',
-    type: 'Import',
-    status: 'NEW',
+    type: 'Debug',
+    status: 'RUNNING',
     options: {
         sourceDir: '/claim-db/sourceFiles',
-        after: ISODate('2014-01-01')
+        after: ISODate('2014-01-01'),
+        claimTypes: {
+            "835": {temp: "claims_835.temp", target: "claims_835", createIndexes: true},
+            "837": {temp: "claims_837.temp", target: "claims_837", createIndexes: true}
+        },
+        archive: {
+            "835a": {temp: "claims_835a.temp", target: "claims_835a.target", createIndexes: true},
+            "837a": {temp: "claims_837a.temp", target: "claims_837a.target", createIndexes: true}
+        },
+        build835c: {
+            "835c": {temp: "claims_835c.temp", target: "claims_835c.target", createIndexes: true},
+            clients: "clientid"
+        }
     }
 });
